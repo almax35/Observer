4590b067de9d493b409109364e71f3fdc0f716ec
almax35 <almaximys@yandex.ru>
2023-05-22 23:48:52 +0300

    first commit

---
+++ src/Deconat.java
@@ -0,0 +1,16 @@
+public class Client {
+    public static void main(String[] args) {
+        Teacher teacher=new Teacher();
+        Group group1 = new Group("g1", teacher);
+        Group group2 = new Group("g2", teacher);
+        Group group3 = new Group("g3", teacher);
+        teacher.estimate();
+        group3.dropOut();
+        teacher.estimate();
+        teacher.estimate();
+        teacher.estimate();
+        teacher.estimate();
+    }
+
+
+}
---
+++ src/Teacher.java
@@ -0,0 +1,16 @@
+public class Theacher implements IObservable{
+    @Override
+    public void RegisterObserver(IObserver o) {
+
+    }
+
+    @Override
+    public void RemoveObserver(IObserver o) {
+
+    }
+
+    @Override
+    public void NotifyObservers() {
+    }
+
+}
